1d3b63ac871458db8b560db9e73e33d51d7a1e62
Alex Hall <alexh25@uw.edu>
2023-06-02 22:23:09 -0700

    updated async for user controller

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5001;
 
-import userRoutes from "./routes/userRoutes.js"; 
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
-app.use('/api/users', userRoutes)
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
