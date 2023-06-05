bf945af535d0738d2be56371cd789fb4408375cd
Alex Hall <alexh25@uw.edu>
2023-06-05 16:54:05 -0700

    created user register controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModels.js";
 
 // auth user and set token
 // route = POST /api/users/auth
@@ -12,7 +13,26 @@ const authUser = asyncHandler(async (req, res) => {
 // access = Public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register User" });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // logout user
@@ -45,4 +65,4 @@ export {
   logoutUser,
   getUserProfile,
   updateUserProfile,
- };
+};
