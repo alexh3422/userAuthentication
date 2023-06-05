e4ad2d048dce6a6e27dff8917a51c879f1f4ed69
Alex Hall <alexh25@uw.edu>
2023-06-05 16:20:09 -0700

    updated routes and controllers for logout, update profile, get profile, and auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,10 +4,45 @@ import asyncHandler from "express-async-handler";
 // route = POST /api/users/auth
 // access = Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(401);
-  throw new Error("There was an error somewhere");
-
   res.status(200).json({ message: "User was Authenticated" });
 });
-//
-export { authUser };
+
+// register user
+// route = POST /api/users/register
+// access = Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// logout user
+// route = POST /api/users/logout
+// access = Public
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// get user profile
+// route = GET /api/users/profile
+// access = Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+// update user profile
+// route = PUT /api/users/profile
+// access = Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+ };
